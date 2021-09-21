d25474a0bf1cd09daef9c8c26164a0a43945c9ec
harkiratsm <71957674+harkiratsm@users.noreply.github.com>
2021-09-21 21:23:17 +0530

    Typo fixed

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json({limit:'50mb'}))
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine","ejs")
-app.get('/',async(req,res)=>{
+app.get('/',(req,res)=>{
     res.render('home')
 })
 
